f628be684eaadd7354d85301811d4ef20811389c
Henry Tseng <henrybear327@gmail.com>
2016-06-04 00:39:18 +1000

    Update Contest setup code

---
+++ contest_setup/Main.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        // Start writing your solution here.
+
+        // Stop writing your solution here.
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class MyScanner 
+    {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner()
+        {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        boolean hasNext()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        String next()
+        {
+            if (hasNext())
+                return st.nextToken();
+            return null;
+        }
+
+        int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
---
+++ contest_setup/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+//LLONG_MIN LLONG_MAX INT_MIN INT_MAX
+
+#ifdef _WIN32
+#define lld "I64d"
+#else
+#define lld "lld"
+#endif
+
+using namespace std;
+
+#define x first
+#define y second
+
+typedef long long int ll;
+typedef pair<int, int> ii;
+
+int main()
+{
+    return 0;
+}
