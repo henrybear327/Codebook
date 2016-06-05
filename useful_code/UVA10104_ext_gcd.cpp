73c5708c77aadb5450f74f4c7d5844bba433eb64
Henry Tseng <henrybear327@gmail.com>
2016-06-05 10:38:50 +1000

    Update ext_gcd

--- useful_code/UVA10104_ext_gcd.cpp
+++ useful_code/UVA10104_ext_gcd.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 typedef long long int ll;
 
-ll ext_gcd(ll a, ll b, ll *x, ll *y)
+ll ext_gcd(ll a, ll b, ll &x, ll &y)
 {
     if (a == 0) {
-        *x = 0;
-        *y = 1;
+        x = 0;
+        y = 1;
         return b;
     }
 
     ll x1, y1;
-    ll gcd = ext_gcd(b % a, a, &x1, &y1);
+    ll gcd = ext_gcd(b % a, a, x1, y1);
 
-    *x = y1 - b / a * x1;
-    *y = x1;
+    x = y1 - (b / a) * x1;
+    y = x1;
 
     return gcd;
 }
@@ -25,7 +25,7 @@ int main()
     ll a, b;
     while (scanf("%lld %lld", &a, &b) == 2) {
         ll gcd, x = 0, y = 1;
-        gcd = ext_gcd(a, b, &x, &y);
+        gcd = ext_gcd(a, b, x, y);
 
         if (a == b)
             x = 0, y = 1;
--- useful_code/algorithm_API.cpp
+++ useful_code/algorithm_API.cpp
@@ -1,16 +1,17 @@
 // return if i is smaller than j
-comp = [&](const T& i, const T& j) -> bool;
+comp = [&](const T &i, const T &j) -> bool;
 vector<T> v;
-bool any_of(v.begin(), v.end(), [&](const T& i) -> bool);
-bool all_of(v.begin(), v.end(), [&](const T& i) -> bool);
+bool any_of(v.begin(), v.end(), [&](const T &i) -> bool);
+bool all_of(v.begin(), v.end(), [&](const T &i) -> bool);
 void copy(inp.begin(), in.end(), out.begin());
 int count(v.begin(), v.end(), int val); // number of val in v
-it unique(v.begin(), v.end()); // it - v.begin() = size
+it unique(v.begin(), v.end());          // it - v.begin() = size
 // after calling, v[nth] will be n-th smallest elem in v
 void nth_element(v.begin(), nth_it, bin_comp);
 void merge(in1.begin(), in1.end(), in2.begin(), in2.end(), out.begin(), comp);
 // include union, intersection, difference, symmetric_difference(xor)
-void set_union(in1.begin(), in1.end(), in2.begin(), in2.end(), out.begin(), comp);
+void set_union(in1.begin(), in1.end(), in2.begin(), in2.end(), out.begin(),
+               comp);
 bool next_permutation(v.begin(), v.end());
 // v1, v2 need sorted already, whether v1 includes v2
 bool inclues(v1.begin(), v1.end(), v2.begin(), v2.end());
