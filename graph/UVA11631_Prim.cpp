8c38df0c12c5eb9ad0a9267a146c879785dfa697
Henry Tseng <henrybear327@gmail.com>
2016-09-29 16:26:33 +0800

    Update Prim algorithm

--- graph/UVA11631_Prim.cpp
+++ graph/UVA11631_Prim.cpp
@@ -10,30 +10,6 @@ typedef pair<int, int> ii;
 #define N 200010
 vector<ii> g[N];
 
-struct UFDS 
-{
-	int par[N];
-	void init(int n) {
-		memset(par, -1, sizeof(int) * n);
-	}
-
-	int root(int x) {
-		return par[x] < 0 ? x : par[x] = root(par[x]);
-	}
-
-	void merge(int x, int y) {
-		x = root(x);
-		y = root(y);
-		if(x == y)
-			return;
-
-		if(par[x] > par[y])
-			swap(x, y);
-		par[x] += par[y];
-		par[y] = x;
-	}
-} uf;
-
 int main()
 {
 	int n, m;
@@ -55,30 +31,26 @@ int main()
 		}
 
 		// prim
+		int ans = 0;
+		bool used[n];
+		memset(used, false, sizeof(used));
+
 		priority_queue< ii, vector<ii>, greater<ii> > pq;
 		pq.push(ii(0, 0)); // push (0, origin)
-		
-		uf.init(n + 1); // init union find
-		int ans = 0, earlyTermination = 0;
 		while(!pq.empty()) {
 			ii cur = pq.top(); 
 			pq.pop();
 			
 			int u = cur.second;
-			if(u != 0 && (uf.root(0) == uf.root(u))) // check loop
+			if(used[u])
 				continue;
-			uf.merge(0, u); 
 			ans += cur.first;
-			earlyTermination++;
-			if(earlyTermination == n) // because of the first node is dummy node
-				break;
+			used[u] = true;
 			
 			for(int i = 0; i < (int)g[u].size(); i++) {
 				int v = g[u][i].first, w = g[u][i].second;
-
-				if(uf.root(0) != uf.root(v)) {
+				if(used[v] == false)
 					pq.push(ii(w, v));
-				}
 			}
 		}
 
--- graph/prim.cpp
+++ graph/prim.cpp
@@ -1,27 +1,23 @@
+int ans = 0;
+bool used[n];
+memset(used, false, sizeof(used));
+
 priority_queue<ii, vector<ii>, greater<ii>> pq;
 pq.push(ii(0, 0)); // push (0, origin)
-
-uf.init(n + 1); // init union find
-int ans = 0, earlyTermination = 0;
 while (!pq.empty())
 {
     ii cur = pq.top();
     pq.pop();
 
     int u = cur.second;
-    if (u != 0 && (uf.root(0) == uf.root(u))) // check loop
+    if (used[u])
         continue;
-    uf.merge(0, u);
     ans += cur.first;
-    earlyTermination++;
-    if (earlyTermination == n) // origin node is dummy node
-        break;
+    used[u] = true;
 
     for (int i = 0; i < (int)g[u].size(); i++) {
         int v = g[u][i].first, w = g[u][i].second;
-
-        if (uf.root(0) != uf.root(v)) {
+        if (used[v] == false)
             pq.push(ii(w, v));
-        }
     }
 }
